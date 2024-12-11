d18057e04fcb60fb6f00db766f22dd01b059e482
babumg1210 <babumg1210@outlook.com>
2024-12-11 18:41:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,25 @@
 variable "resource_group_name" {
-  description = "provide the resource group name "
+  description = "Provide the resource group name"
   type        = string
-  default = "test-rg12"
+  default     = "test-rg12"
 }
+
 variable "resource_group_location" {
-  description = "provide the location of resource group"
+  description = "Provide the location of the resource group"
   type        = string
-  default = "west us"
+  default     = "west us"
 }
 
 # Random String Resource
 resource "random_string" "myrandom" {
-  length = 6
-  upper = false 
+  length  = 6
+  upper   = false 
   special = false
   numeric = false   
 }
+
 module "finalrg" { 
-        source = "./modules/" 
-        resource_group_name = var.resource_group_name
-        resource_group_location = var.resource_group_location
-} 
+  source = "./modules"  # Ensure this path is correct
+  resource_group_name = "${var.resource_group_name}-${random_string.myrandom.result}"  # Use the random string
+  resource_group_location = var.resource_group_location
+}
