2dd6e9bf2cb72a2a78ff5ee12f25959ac6687a78
babumg1210 <babumg1210@outlook.com>
2024-12-11 14:09:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
 provider "azurerm" {
   features {}
+ # Optionally, you can specify the client ID, client secret, tenant ID, and subscription ID directly
+  client_id       = var.servicePrincipalId
+  client_secret   = var.servicePrincipalKey
+  tenant_id       = var.tenantId
+  subscription_id  = var.subscriptionId
 }
 
 module "my_resource_group" {
