60d820e7f428001ee3735de229b2849ed5b08575
babumg1210 <babumg1210@outlook.com>
2024-12-11 14:03:26 +0530

    Delete modules/resource-group directory

--- modules/resource-group/variable.tf
+++
@@ -1,9 +0,0 @@
-variable "resource_group_name" {
-  description = "The name of the resource group."
-  type        = string
-}
-
-variable "location" {
-  description = "The location/region where the resource group will be created."
-  type        = string
-}
