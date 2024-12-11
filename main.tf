e48f1c64c2423bbfd56f478d9fc7f64320199ecb
babumg1210 <babumg1210@outlook.com>
2024-12-11 18:07:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 variable "resource_group_name" {
   description = "provide the resource group name "
   type        = string
-  default = "test-rg3"
+  #default = "test-rg3"
 }
 variable "resource_group_location" {
   description = "provide the location of resource group"
@@ -13,6 +13,6 @@ variable "resource_group_location" {
 
 module "finalrg" { 
         source = "./modules/" 
-        resource_group_name = var.resource_group_name
+        resource_group_name = "test--${random_string.myrandom.id}"
         resource_group_location = var.resource_group_location
 } 
