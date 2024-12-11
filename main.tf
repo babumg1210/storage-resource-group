2b6111915dc68085f32797af77b7bb90247b37ff
babumg1210 <babumg1210@outlook.com>
2024-12-11 19:11:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,15 +52,6 @@ module "finalrg" {
   source = "./modules"  # Ensure this path is correct
   resource_group_name = "${var.resource_group_name}-${random_string.myrandom.result}"  # Use the random string
   resource_group_location = var.resource_group_location
-}
-module "storage_account" {
-  source              = "./Storage-account"
-  #client_id       = "5eccd7fc-aed1-446a-a81e-aa66c5584316"  # appId from the JSON output
-  #client_secret   = "bz88Q~4OJ~O-.67ptF4Sbyf6~wEP-d9SEyTk9adPv"  # password from the JSON output
-  #tenant_id       = "c7de2ec6-e7a9-41e7-99e8-e6ff52f3a8d0"  # tenant from the JSON output
-  #subscription_id = "ae1b5111-4f4f-477d-b4d4-419fd8cbc789"  # Your Azure subscription ID
-  resource_group_name = "${var.resource_group_name}-${random_string.myrandom.result}"  # Use the random string
-  resource_group_location = var.resource_group_location
   name =                  var.storage_account_name
   environment          =  var.environment
   vnet_name            =  "${var.vnet_name}-${random_string.myrandom.result}"
