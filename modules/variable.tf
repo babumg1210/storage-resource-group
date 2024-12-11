ea72b1703eaa256e7493572425f5e76320c0a146
babumg1210 <babumg1210@outlook.com>
2024-12-11 19:08:54 +0530

    Update variable.tf

--- modules/variable.tf
+++ modules/variable.tf
@@ -2,12 +2,12 @@
 variable "resource_group_name" {
   description = "provide the resource group name "
   type        = string
-  default     = "test-rg"
+  #default     = "test-rg"
 }
 variable "resource_group_location" {
   description = "provide the location of resource group"
   type        = string
-  default     = "west us"
+  #default     = "west us"
 }
 
 variable "environment" {
