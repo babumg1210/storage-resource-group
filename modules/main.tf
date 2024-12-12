2c2943ed3bf9d6c8ded84584022f0e964b10e7ac
babumg1210 <babumg1210@outlook.com>
2024-12-12 07:52:15 +0530

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -16,15 +16,17 @@ resource "azurerm_storage_account" "storage_account" {
   }
 }
 resource "azurerm_virtual_network" "vnet" {
-  name                = "${local.resource_name_prefix}-${var.vnet_name}"
+  name                = "${var.vnet_name}-${random_string.myrandom.result}"
   address_space       = var.vnet_address_space
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  tags = local.common_tags
+  tags = {
+    environment = var.environment
+  }
 }
 # Resource-1: Create WebTier Subnet
 resource "azurerm_subnet" "websubnet" {
-  name                 = "${azurerm_virtual_network.vnet.name}-${var.web_subnet_name}"
+  name                 = var.web_subnet_name
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = var.web_subnet_address  
