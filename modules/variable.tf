16f4e0d5608d7d8d02c8c657e979fdd2f31068e0
babumg1210 <babumg1210@outlook.com>
2024-12-12 07:50:35 +0530

    Update variable.tf

--- modules/variable.tf
+++ modules/variable.tf
@@ -22,23 +22,23 @@ variable "storage_account_name" {
 variable "vnet_name" {
   description = "Virtual Network name"
   type = string
-  default = "vnet-default"
+  #default = "vnet-default"
 }
 variable "vnet_address_space" {
   description = "Virtual Network address_space"
   type = list(string)
-  default = ["10.0.0.0/16"]
+ # default = ["10.0.0.0/16"]
 }
 # Web Subnet Name
 variable "web_subnet_name" {
   description = "Virtual Network Web Subnet Name"
   type = string
-  default = "websubnet"
+  #default = "websubnet"
 }
 # Web Subnet Address Space
 variable "web_subnet_address" {
   description = "Virtual Network Web Subnet Address Spaces"
   type = list(string)
-  default = ["10.0.1.0/24"]
+  #default = ["10.0.1.0/24"]
 }
 
